28a40c810b99f15a43d11038ecfb9be731ca3755
Jeevan-27 <141016628+Jeevan-27@users.noreply.github.com>
2025-04-29 09:01:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
-    origin: ["https://threads-seven-omega.vercel.app"],
+    origin: ["https://threads-bay-delta.vercel.app/"],
     methods: ["POST","GET"],
     credentials: true
   }
